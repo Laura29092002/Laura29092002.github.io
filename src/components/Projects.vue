741c4c00d8092ddfc93bf87dd65f51d95dcff05f
laura2909 <mussardlaura299@gmail.com>
2025-12-12 10:21:57 +0100

    add navigation to swipper

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -4,16 +4,35 @@ import { Swiper, SwiperSlide } from 'swiper/vue'
 import 'swiper/css'
 import 'swiper/css/effect-coverflow'
 import 'swiper/css/pagination'
+import 'swiper/css/navigation'
 import { projectData } from '@/staticData/projectData'
 import type { Swiper as SwiperType } from 'swiper'
 
-import { Autoplay, EffectCoverflow, Pagination } from 'swiper/modules'
+import { A11y, Autoplay, EffectCoverflow, Keyboard, Navigation, Pagination } from 'swiper/modules'
 import Modal from './Modal.vue'
 import type { projectType } from '@/types/projectType'
 
 const pagination = { clickable: true }
-const modules = [Autoplay, EffectCoverflow, Pagination]
+const modules = [Autoplay, EffectCoverflow, Pagination, Keyboard, A11y, Navigation]
 const autoplay = {delay: 5000, disableOnInteraction: false}
+const keyboard = {enabled: true, onlyInViewport: false, pageUpDown: true}
+const navigation = {
+  nextEl: '.swiper-button-next',
+  prevEl: '.swiper-button-prev',
+}
+
+const a11y = {
+  enabled: true,
+  containerMessage: 'Carrousel des projets',
+  containerRoleDescriptionMessage: 'Slider interactif',
+  itemRoleDescriptionMessage: 'Diapositive',
+  slideLabelMessage: 'Slide {{index}} sur {{slidesLength}}',
+  prevSlideMessage: 'Slide précédente',
+  nextSlideMessage: 'Slide suivante',
+  firstSlideMessage: 'Première slide',
+  lastSlideMessage: 'Dernière slide',
+  paginationBulletMessage: 'Aller à la slide {{index}}'
+}
 
 const popupVisible = ref(false)
 const selectedSlide = ref({})
@@ -51,6 +70,9 @@ function closeModal() {
         :centeredSlides="true"
         :slidesPerView="'auto'"
         :autoplay="autoplay"
+        :keyboard="keyboard"
+        :navigation="navigation"
+        :a11y="a11y"
         :coverflowEffect="{
           rotate: 50,
           stretch: 0,
@@ -71,6 +93,8 @@ function closeModal() {
           <img :src="slide.image" :alt="slide.title" />
           <p>{{ slide.title }}</p>
         </swiper-slide>
+      <div class="swiper-button-prev"></div>
+      <div class="swiper-button-next"></div>
       </swiper>
       <Modal v-model:popupVisible="popupVisible" :selectedSlide="selectedSlide" @update:popupVisible="closeModal"/>
     </div>
@@ -112,6 +136,20 @@ function closeModal() {
   cursor: pointer;
   transition: transform 0.3s ease;
 }
+
+.swiper-button-prev,
+.swiper-button-next {
+  color: var(--primary-color);
+  padding: 10px;
+}
+
+
+.swiper-button-prev:hover,
+.swiper-button-next:hover {
+  background-color: var(--shadow);
+  border-radius: 50%;
+}
+
 
 @media (max-width: 630px) {
   .swiper-slide {
